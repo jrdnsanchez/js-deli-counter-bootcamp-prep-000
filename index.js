4b434e7e35dbe25eae1cb5c4794826822ad14db5
Jordan Sanchez <jrdnsanchez@gmail.com>
2019-09-03 20:13:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+var i = 0; 
+
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
   }
+function  takeANumber(katzDeliLine){
+  i++
+  katzDeliLine.push(i);
+  return `Welcome you have ticket number ${i}.`;
+}  
 function nowServing (katzDeliLine){
   if (katzDeliLine.length>0) return `Currently serving ${katzDeliLine.shift()}.`;
   else return `There is nobody waiting to be served!`;
